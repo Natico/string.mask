8e13b01d7deb40a48bcdb0ac4507202f6251a7d6
natico <natamjadian.n@gmail.com>
2019-06-01 20:16:45 +0430

    first

---
+++ js/string.mask.js
@@ -0,0 +1,15 @@
+/**
+ * Auther: Nasser Amjadian
+ * email: amjadian.n@gmail.com
+ *
+ * 
+ * */
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+
+var stringMask = function(string, mask){
+    
+}
